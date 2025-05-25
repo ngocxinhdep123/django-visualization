cbd06aa097456a33b2142b2e83163b3f7c6a6626
ngocxinhdep123 <truongthiminhngoc0110@gmail.com>
2025-05-25 21:02:10 +0700

    remove pywin32 for render deploy

--- data_visualization/settings.py
+++ data_visualization/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static'] 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
